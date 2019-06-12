29b8cffd7b1261b70eceed4b7846f403170e81e6
Maciek Kostrzycki <m.kostrzycki@global4net.com>
2019-06-12 22:19:34 +0200

    #TASK-6 Added collection to config. Changed path for Solarium 5. Refactor

--- src/Console/Command/TestCommand.php
+++ src/Console/Command/TestCommand.php
@@ -59,14 +59,15 @@ class TestCommand extends Command
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $host = '10.20.0.1';
-        $port = 6648;
+        $host = '10.0.0.21';
+        $port = 9090;
         $path = '/solr/';
+        $collection = '';
         $core = 'qa';
 
         
 
-        $config = new Config(Config::ENGINE_SOLR, $host, $port, $path, $core, 10, false);
+        $config = new Config(Config::ENGINE_SOLR, $host, $port, $path, $collection, $core, 10, false);
         $config->setEngine($config::ENGINE_SOLR);
         
         $puller = new Puller($config);
@@ -132,6 +133,6 @@ class TestCommand extends Command
 
         // indexer tworzy pusher na podstawie konfiguracji (solr) przekazując mu konfigurację w konstruktorze i foreach Documents odpala query?
 
-        $output->writeln("Found: " . $response->getNumFound());
+        $output->writeln("Found: " . $result->getNumFound());
     }
 }
