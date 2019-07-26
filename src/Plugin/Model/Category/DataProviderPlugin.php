cbc79f86d482e045f9114e19d982265a2e07fba6
Choinek <adrian@chojnicki.pl>
2019-07-26 23:21:29 +0200

    + add event dispatch for category facets prepare

--- src/Plugin/Model/Category/DataProviderPlugin.php
+++ src/Plugin/Model/Category/DataProviderPlugin.php
@@ -5,8 +5,11 @@ namespace G4NReact\MsCatalogMagento2\Plugin\Model\Category;
 use G4NReact\MsCatalogMagento2\Model\Attribute\ReactStoreFrontFilters;
 use G4NReact\MsCatalogMagento2\Model\Config\Source\AttributesReactFilter;
 use Magento\Catalog\Model\Category\DataProvider;
-use Magento\Framework\App\RequestInterface;
+use Magento\Framework\Event\Manager as EventManager;
+use Magento\Framework\Exception\InputException;
+use Magento\Framework\Exception\NoSuchEntityException;
 use Magento\Framework\Serialize\SerializerInterface;
+use Zend\Mail\Header\Subject;
 
 /**
  * Class DataProviderPlugin
@@ -33,21 +36,28 @@ class DataProviderPlugin
     protected $serializer;
 
     /**
+     * @var EventManager
+     */
+    protected $eventManager;
+
+    /**
      * DataProviderPlugin constructor.
      *
      * @param ReactStoreFrontFilters $reactStoreFrontFilters
      * @param AttributesReactFilter $attributesReactFilter
      * @param SerializerInterface $serializer
+     * @param EventManager $eventManager
      */
     public function __construct(
         ReactStoreFrontFilters $reactStoreFrontFilters,
         AttributesReactFilter $attributesReactFilter,
-        SerializerInterface $serializer
-    )
-    {
+        SerializerInterface $serializer,
+        EventManager $eventManager
+    ) {
         $this->reactStoreFrontFilters = $reactStoreFrontFilters;
         $this->attributesReactFilter = $attributesReactFilter;
         $this->serializer = $serializer;
+        $this->eventManager = $eventManager;
     }
 
     /**
@@ -55,11 +65,12 @@ class DataProviderPlugin
      * @param $result
      *
      * @return mixed
+     * @throws InputException
      */
     public function afterPrepareMeta(DataProvider $subject, $result)
     {
         $result['react_storefront_filters'] = [
-            'children' => $this->prepareAttributesFields()
+            'children' => $this->prepareAttributesFields($subject)
         ];
 
         return $result;
@@ -70,14 +81,15 @@ class DataProviderPlugin
      * @param $result
      *
      * @return mixed
+     * @throws NoSuchEntityException
      */
     public function afterGetData(DataProvider $subject, $result)
     {
         $category = $subject->getCurrentCategory();
         $categoryId = $category->getId();
         $filters = $category->getReactStorefrontFilters();
-        
-        if($filters) {
+
+        if ($filters) {
             $filters = $this->serializer->unserialize($filters);
             foreach ($filters[AttributesReactFilter::FACETS] as $facets) {
                 $result[$categoryId][$facets . self::REACT_STOREFRONT_FILTERS_SUFFIX][0] = AttributesReactFilter::FACETS;
@@ -95,13 +107,16 @@ class DataProviderPlugin
     /**
      * Prepare by category attributes
      *
+     * @param Subject $subject
      * @return array
+     * @throws InputException
      */
-    protected function prepareAttributesFields()
+    protected function prepareAttributesFields(DataProvider $subject)
     {
         $res = [];
         foreach ($this->reactStoreFrontFilters->getProductAttributes() as $attribute) {
-            $res[$attribute->getAttributeCode() . self::REACT_STOREFRONT_FILTERS_SUFFIX] = [
+            $object = new \stdClass();
+            $object->data = [
                 'arguments' => [
                     'data' => [
                         'name' => $attribute->getAttributeCode() . self::REACT_STOREFRONT_FILTERS_SUFFIX,
@@ -118,6 +133,12 @@ class DataProviderPlugin
                     ]
                 ]
             ];
+
+            $this->eventManager->dispatch('mscatalog_magento2_adminhtml_plugin_category_attributes_prepare_before', ['resource' => $object, 'subject' => $subject]);
+
+            if ($object->data) {
+                $res[$attribute->getAttributeCode() . self::REACT_STOREFRONT_FILTERS_SUFFIX] = $object->data;
+            }
         }
 
         return $res;
