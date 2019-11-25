2d7bb7edfbe8137d008c2a32485ca4280b09f5df
nmalecki <n.malecki@global4net.com>
2019-11-25 11:06:19 +0100

    #MS-catalog-401 fix imrpove facets to handle array and int

--- src/Helper/Facets.php
+++ src/Helper/Facets.php
@@ -5,9 +5,12 @@ namespace G4NReact\MsCatalogMagento2\Helper;
 use Exception;
 use Magento\Catalog\Model\Category;
 use Magento\Catalog\Model\CategoryRepository;
+use Magento\Catalog\Model\ResourceModel\Category\CollectionFactory as CategoryCollectionFactory;
 use Magento\Framework\App\Helper\AbstractHelper;
 use Magento\Framework\App\Helper\Context;
 use Magento\Framework\Exception\NoSuchEntityException;
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Catalog\Model\ResourceModel\Category\Collection as CategoryCollection;
 
 /**
  * Class Facets
@@ -21,9 +24,9 @@ class Facets extends AbstractHelper
     protected $queryHelper;
 
     /**
-     * @var CategoryRepository
+     * @var CategoryCollectionFactory
      */
-    protected $categoryRepository;
+    protected $categoryCollectionFactory;
 
     /**
      * Facets constructor
@@ -34,92 +37,107 @@ class Facets extends AbstractHelper
      */
     public function __construct(
         Context $context,
-        CategoryRepository $categoryRepository,
-        Query $queryHelper
-    ) {
-        $this->categoryRepository = $categoryRepository;
+        Query $queryHelper,
+        CategoryCollectionFactory $categoryCollectionFactory
+    )
+    {
         $this->queryHelper = $queryHelper;
+        $this->categoryCollectionFactory = $categoryCollectionFactory;
 
         parent::__construct($context);
     }
 
     /**
-     * @param $categoryId
+     * @param int|array $categoryId
      * @return array
      */
-    public function getFacetFieldsByCategory(int $categoryId)
+    public function getFacetFieldsByCategory($needleCategoryId)
     {
         $facetFields = [];
+        $categoryIds = is_array($needleCategoryId) ? $needleCategoryId : [$needleCategoryId];
+        $categories = $this->getCategories($categoryIds);
+        /** @var Category $category */
+        foreach ($categories as $category) {
+            try {
+                $filters = json_decode($category->getData('react_storefront_filters'), true);
 
-        try {
-            /** @var Category $category */
-            $category = $this->categoryRepository->get($categoryId);
-            $filters = json_decode($category->getData('react_storefront_filters'), true);
-
-            if (isset($filters['facets'])) {
-                foreach ($filters['facets'] as $facet) {
-                    if ($field = $this->queryHelper->getFieldByAttributeCode($facet)) {
-                        $facetFields[$field->getName()] = $field;
+                if (isset($filters['facets'])) {
+                    foreach ($filters['facets'] as $facet) {
+                        if ($field = $this->queryHelper->getFieldByAttributeCode($facet)) {
+                            $facetFields[$field->getName()] = $field;
+                        }
                     }
                 }
-            }
 
-        } catch (NoSuchEntityException $noSuchEntityException){
-            $this->_logger->error(
-                'g4n-react-ms-catalog-magento-helper-facets',
-                [
-                    'exception' => 'Category with id: ' . $categoryId . ' doesnt exist!'
-                ]
-            );
-        } catch (Exception $exception) {
-            $this->_logger->error(
-                'g4n-react-ms-catalog-magento2',
-                [
-                    'exception' => $exception->getMessage()
-                ]
-            );
+            } catch (NoSuchEntityException $noSuchEntityException) {
+                $this->_logger->error(
+                    'g4n-react-ms-catalog-magento-helper-facets',
+                    [
+                        'exception' => 'Category with id: ' . $category->getId() . ' doesnt exist!'
+                    ]
+                );
+            } catch (Exception $exception) {
+                $this->_logger->error(
+                    'g4n-react-ms-catalog-magento2',
+                    [
+                        'exception' => $exception->getMessage()
+                    ]
+                );
+            }
         }
 
         return $facetFields;
     }
 
     /**
-     * @param $categoryId
+     * @param array $ids
+     * @return CategoryCollection
+     * @throws LocalizedException
+     */
+    protected function getCategories(array $ids)
+    {
+        return $this->categoryCollectionFactory->create()
+            ->addIdFilter($ids)
+            ->addAttributeToSelect('react_storefront_filters');
+    }
+
+    /**
+     * @param array|int $categoryId
      * @return array
      */
-    public function getStatsFieldsByCategory(int $categoryId)
+    public function getStatsFieldsByCategory($needleCategoryId)
     {
         $facetFields = [];
+        $categoryIds = is_array($needleCategoryId) ? $needleCategoryId : [$needleCategoryId];
+        $categories = $this->getCategories($categoryIds);
+        /** @var Category $category */
+        foreach ($categoryIds as $categoryId) {
+            try {
+                $filters = json_decode($category->getData('react_storefront_filters'), true);
 
-        try {
-            /** @var Category $category */
-            $category = $this->categoryRepository->get($categoryId);
-            $filters = json_decode($category->getData('react_storefront_filters'), true);
-
-            if (isset($filters['stats'])) {
-                foreach ($filters['stats'] as $stat) {
-                    if ($field = $this->queryHelper->getFieldByAttributeCode($stat)) {
-                        $facetFields[$field->getName()] = $field;
+                if (isset($filters['stats'])) {
+                    foreach ($filters['stats'] as $stat) {
+                        if ($field = $this->queryHelper->getFieldByAttributeCode($stat)) {
+                            $facetFields[$field->getName()] = $field;
+                        }
                     }
                 }
-            }
 
-        }
-        catch (NoSuchEntityException $noSuchEntityException){
-            $this->_logger->error(
-                'g4n-react-ms-catalog-magento-helper-facets',
-                [
-                    'exception' => 'Category with id: ' . $categoryId . ' doesnt exist!'
-                ]
-            );
-        }
-        catch (Exception $exception) {
-            $this->_logger->error(
-                'g4n-react-ms-catalog-magento2',
-                [
-                    'exception' => $exception->getMessage()
-                ]
-            );
+            } catch (NoSuchEntityException $noSuchEntityException) {
+                $this->_logger->error(
+                    'g4n-react-ms-catalog-magento-helper-facets',
+                    [
+                        'exception' => 'Category with id: ' . $category->getId() . ' doesnt exist!'
+                    ]
+                );
+            } catch (Exception $exception) {
+                $this->_logger->error(
+                    'g4n-react-ms-catalog-magento2',
+                    [
+                        'exception' => $exception->getMessage()
+                    ]
+                );
+            }
         }
 
         return $facetFields;
