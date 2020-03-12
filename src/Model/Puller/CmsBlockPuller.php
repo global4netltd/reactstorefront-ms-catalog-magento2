708fe97e8573a2db0de2da3d6627afb162c728f9
sofia <s.andrianow@global4net.com>
2020-03-12 14:56:22 +0100

    fix for store id in cms blocks and pages

--- src/Model/Puller/CmsBlockPuller.php
+++ src/Model/Puller/CmsBlockPuller.php
@@ -114,6 +114,14 @@ class CmsBlockPuller extends AbstractPuller
             );
         }
 
+        if ($storeIdField = $document->getField('store_id')) {
+            if (is_array($storeIdField->getValue())) {
+                $storeIdField->setValue($storeId);
+                $storeIdField->setType(Document\Field::FIELD_TYPE_INT);
+                $storeIdField->setMultiValued(false);
+            }
+        }
+
         $eventData = [
             'cms_block' => $cmsBlock,
             'document' => $document,
