6d5053065e394e6a18c863f1c97fdd4d90eab427
Maciek Kostrzycki <maciej.kostrzycki@gmail.com>
2020-04-06 18:01:22 +0200

    Added store id param to cli commands.

--- src/Console/Command/ReindexAllCmsBlock.php
+++ src/Console/Command/ReindexAllCmsBlock.php
@@ -4,6 +4,7 @@ namespace G4NReact\MsCatalogMagento2\Console\Command;
 
 use G4NReact\MsCatalogMagento2\Model\Indexer\AbstractIndexer;
 use G4NReact\MsCatalogMagento2\Model\Indexer\CmsBlockIndexer;
+use Magento\Store\Model\StoreManagerInterface;
 use Symfony\Component\Console\Input\InputOption;
 
 /**
@@ -20,15 +21,17 @@ class ReindexAllCmsBlock extends AbstractReindex
     /**
      * ReindexAllCmsBlock constructor.
      *
+     * @param StoreManagerInterface $storeManager
      * @param CmsBlockIndexer $cmsBlockIndexer
      * @param string|null $name
      */
     public function __construct(
+        StoreManagerInterface $storeManager,
         CmsBlockIndexer $cmsBlockIndexer,
         string $name = null
     ) {
         $this->cmsBlockIndexer = $cmsBlockIndexer;
-        parent::__construct($name);
+        parent::__construct($storeManager, $name);
     }
 
     /**
@@ -67,6 +70,13 @@ class ReindexAllCmsBlock extends AbstractReindex
                 self::REQUIRED_OPTION_INFO,
                 true
             ),
+            new InputOption(
+                self::INPUT_OPTION_STORE_ID,
+                null,
+                InputOption::VALUE_OPTIONAL,
+                self::REQUIRED_OPTION_INFO,
+                false
+            )
         ];
     }
 
--- src/Console/Command/ReindexAllProduct.php
+++ src/Console/Command/ReindexAllProduct.php
@@ -4,6 +4,7 @@ namespace G4NReact\MsCatalogMagento2\Console\Command;
 
 use G4NReact\MsCatalogMagento2\Model\Indexer\AbstractIndexer;
 use G4NReact\MsCatalogMagento2\Model\Indexer\ProductIndexer;
+use Magento\Store\Model\StoreManagerInterface;
 use Symfony\Component\Console\Input\InputOption;
 
 /**
@@ -20,15 +21,17 @@ class ReindexAllProduct extends AbstractReindex
     /**
      * ReindexAllProduct constructor
      *
+     * @param StoreManagerInterface $storeManager
      * @param ProductIndexer $productIndexer
      * @param string|null $name
      */
     public function __construct(
+        StoreManagerInterface $storeManager,
         ProductIndexer $productIndexer,
         string $name = null
     ) {
         $this->productIndexer = $productIndexer;
-        parent::__construct($name);
+        parent::__construct($storeManager, $name);
     }
 
     /**
@@ -67,6 +70,13 @@ class ReindexAllProduct extends AbstractReindex
                 self::REQUIRED_OPTION_INFO,
                 true
             ),
+            new InputOption(
+                self::INPUT_OPTION_STORE_ID,
+                null,
+                InputOption::VALUE_OPTIONAL,
+                self::REQUIRED_OPTION_INFO,
+                false
+            )
         ];
     }
 
